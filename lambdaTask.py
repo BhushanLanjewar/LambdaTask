9d021af1529cffbb344331f797efd345e6b3be90
BhushanLanjewar <167945169+BhushanLanjewar@users.noreply.github.com>
2024-07-30 21:36:20 +0530

    Create lambdaTask.py

---
+++ lambdaTask.py
@@ -0,0 +1,41 @@
+# Additionn
+add = lambda x,y: x+y 
+
+print('Addition =',add(5,3))
+
+# Subtraction
+sub = lambda x,y: x-y 
+
+print('Subtraction =',sub(100,50))
+
+# Multiplication
+multi = lambda x,y: x*y 
+
+print('Multiplication =',multi(10,3))
+
+# Division
+div = lambda x,y: x/y 
+
+print('Division =',div(1000,10))
+
+# Square
+square = lambda x: x * x 
+
+print('Square =',square(10))
+
+# Cube
+cube = lambda x: x * x * x
+
+print('Cube =',cube(10))
+
+# Even
+even = lambda x: 'Even' if (x%2 == 0) else 'Odd'
+
+print('Number =',even(10))
+
+# Lambda function
+number = lambda x,y : x if (x>y) else y
+
+print('Greater =',number(5,1))
+
+
